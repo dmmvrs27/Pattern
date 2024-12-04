38f8661bc206ee9377e66c41af3483f374ec54de
dmmvrs27 <MrVirus27062002@gmail.com>
2024-12-05 01:12:34 +0300

    lr 2.2(5) / Added class Data_list_student_short()

--- Data_table.kt
+++ Data_table.kt
@@ -10,7 +10,7 @@ class Data_table<T>(private val data: Array<Array<T>>) {
     fun getRowCount(): Int = data.size
 }
 
-abstract class Data_list<T : Comparable<T>>(private val elements: Array<T>){
+abstract class Data_list<T : Comparable<T>>(protected val elements: Array<T>){
     private val selected = mutableSetOf<Int>()
     init {
         require(elements.isNotEmpty()) { "Массив не может быть пустой!" }
@@ -23,4 +23,16 @@ abstract class Data_list<T : Comparable<T>>(private val elements: Array<T>){
     fun getSelected() = selected.toIntArray()
     abstract fun getNames(): Array<String>
     abstract fun getData(): Data_table<T>
+}
+
+class Data_list_student_short(elements: Array<Student_short>) : Data_list<Student_short>(elements) {
+    override fun getNames(): Array<String> {
+        return arrayOf("Фамилия И.О.", "Гит", "Контакт")
+    }
+    override fun getData(): Data_table<Student_short> {
+        val data = elements.map { student ->
+            arrayOf(student)
+        }.toTypedArray()
+        return Data_table(data)
+    }
 }
